3e2d0ecd14a23c190cc42a91feb44fac07b65415
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-14 18:02:13 +0300

    update Customer

--- src/main/java/org/acme/model/Customer.java
+++ src/main/java/org/acme/model/Customer.java
@@ -25,7 +25,7 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @NotNull
     @Size(min = 9, max = 9)
     @Column(nullable = false, unique = true)
