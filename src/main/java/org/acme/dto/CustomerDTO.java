6db83e6cd168779abbe505d800b670bb14d37c91
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-14 17:58:48 +0300

    Update CustomerDTO.java

--- src/main/java/org/acme/dto/CustomerDTO.java
+++ src/main/java/org/acme/dto/CustomerDTO.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.acme.dto;
 
-/**
- *
- * @author stavroulabakogianni
- */
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CustomerDTO {
-    
+
+    @NotNull
+    @Size(min = 9, max = 9)
+    @Column(nullable = false, unique = true)
+    private String vat;
+
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "first_name", length = 30)
+    private String firstName;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Size(max = 80)
+    @Column(unique = true, name = "email", length = 80)
+    private String email;
+
+    @Size(max = 20)
+    @Column(name = "mobile_phone", length = 20)
+    private String mobilePhone;
+
 }
