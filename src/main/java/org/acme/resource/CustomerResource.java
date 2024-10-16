cada76f64e2d310e3f72676779cc37031dc2d9f6
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-16 17:29:50 +0300

    Update CustomerResource.java

--- src/main/java/org/acme/resource/CustomerResource.java
+++ src/main/java/org/acme/resource/CustomerResource.java
@@ -2,18 +2,17 @@ package org.acme.resource;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.acme.dto.CustomerDTO;
 import org.acme.rest.client.ValidateService;
+import org.acme.rest.client.ValidateVatResponse;
 import org.acme.service.CustomerServiceImpl;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -30,12 +29,6 @@ public class CustomerResource {
 	@RestClient
 	ValidateService validateService;
 
-	@GET
-	@Path("/vat/{vat}")
-	public Boolean id(@PathParam("vat") String vat) {
-		return validateService.checkVat(vat);
-	}
-
 	@Path("/customers")
 	@GET
 	public List<CustomerDTO> getCustomers() {
@@ -51,7 +44,9 @@ public class CustomerResource {
 	@Path("/customer")
 	@POST
 	public boolean saveCustomer(CustomerDTO customerDTO) {
-		if (validateService.checkVat(customerDTO.getVat()));
+            
+            ValidateVatResponse response = validateService.checkVat("EL", customerDTO.getVat());
+		if (response.getValid());
 		{
 			if (customerDTO == null && (customerServiceImpl.findByVat(customerDTO.getVat()).isPresent())) {
 				return false;
