069bbf320462e38b35588fcd55e3922ad61d7a55
alexandrosaristeridis <alexandrosaristeridis@aristeridis1-nb>
2024-10-16 13:46:52 +0300

    post customer on resource

--- src/main/java/org/acme/dto/CustomerDTO.java
+++ src/main/java/org/acme/dto/CustomerDTO.java
@@ -4,32 +4,46 @@ import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class CustomerDTO {
 
-    @NotNull
-    @Size(min = 9, max = 9)
-    @Column(nullable = false, unique = true)
-    private String vat;
+	@NotNull
+	@Size(min = 9, max = 9)
+	@Column(nullable = false, unique = true)
+	private String vat;
 
-    @NotNull
-    @Size(max = 30)
-    @Column(name = "first_name", length = 30)
-    private String firstName;
+	public String getVat() {
+		return vat;
+	}
 
-    @NotNull
-    @Size(max = 50)
-    @Column(name = "last_name", length = 50)
-    private String lastName;
+	public void setVat(String vat) {
+		this.vat = vat;
+	}
 
-    @NotNull
-    @Email
-    @Size(max = 80)
-    @Column(unique = true, name = "email", length = 80)
-    private String email;
+	@NotNull
+	@Size(max = 30)
+	@Column(name = "first_name", length = 30)
+	private String firstName;
 
-    @Size(max = 20)
-    @Column(name = "mobile_phone", length = 20)
-    private String mobilePhone;
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "last_name", length = 50)
+	private String lastName;
+
+	@NotNull
+	@Email
+	@Size(max = 80)
+	@Column(unique = true, name = "email", length = 80)
+	private String email;
+
+	@Size(max = 20)
+	@Column(name = "mobile_phone", length = 20)
+	private String mobilePhone;
 
 }
--- src/main/java/org/acme/resource/CustomerResource.java
+++ src/main/java/org/acme/resource/CustomerResource.java
@@ -6,6 +6,7 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,20 +18,32 @@ import org.acme.service.CustomerServiceImpl;
 @Consumes(MediaType.APPLICATION_JSON)
 @RequestScoped
 public class CustomerResource {
-    
-    @Inject 
-    CustomerServiceImpl customerServiceImpl;
-    
-    @Path("/customers")
-    @GET
-    public List<CustomerDTO> getCustomers (){
-        return customerServiceImpl.findAll();
-    }
-    
-    @Path("/{vat}")
-    @GET
-    public Optional<CustomerDTO> getCustomerByVat(String vat) {
-        return customerServiceImpl.findByVat(vat);
-    }
-    
+
+	@Inject
+	CustomerServiceImpl customerServiceImpl;
+
+	@Path("/customers")
+	@GET
+	public List<CustomerDTO> getCustomers() {
+		return customerServiceImpl.findAll();
+	}
+
+	@Path("/{vat}")
+	@GET
+	public Optional<CustomerDTO> getCustomerByVat(String vat) {
+		return customerServiceImpl.findByVat(vat);
+	}
+
+	@Path("/customer")
+	@POST
+	public boolean saveCustomer(CustomerDTO customerDTO) {
+		if (customerDTO == null && (customerServiceImpl.findByVat(customerDTO.getVat()).isPresent())) {
+			return false;
+
+		} else {
+			customerServiceImpl.saveCustomer(customerDTO);
+			return true;
+		}
+	}
+
 }
