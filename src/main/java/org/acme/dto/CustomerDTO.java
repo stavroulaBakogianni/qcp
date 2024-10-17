a3ba4399b654c52cfa44176b1c0cc094bcc3513e
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-10-17 18:01:03 +0300

    Remove annotation Column

--- src/main/java/org/acme/dto/CustomerDTO.java
+++ src/main/java/org/acme/dto/CustomerDTO.java
@@ -13,37 +13,33 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CustomerDTO {
 
-	@NotNull
-	@Size(min = 9, max = 9)
-	@Column(nullable = false, unique = true)
-	private String vat;
+    @NotNull
+    @Size(min = 9, max = 9)
+    @Column(nullable = false, unique = true)
+    private String vat;
 
-	public String getVat() {
-		return vat;
-	}
+    public String getVat() {
+        return vat;
+    }
 
-	public void setVat(String vat) {
-		this.vat = vat;
-	}
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
 
-	@NotNull
-	@Size(max = 30)
-	@Column(name = "first_name", length = 30)
-	private String firstName;
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
 
-	@NotNull
-	@Size(max = 50)
-	@Column(name = "last_name", length = 50)
-	private String lastName;
+    @NotNull
+    @Size(max = 50)
+    private String lastName;
 
-	@NotNull
-	@Email
-	@Size(max = 80)
-	@Column(unique = true, name = "email", length = 80)
-	private String email;
+    @NotNull
+    @Email
+    @Size(max = 80)
+    private String email;
 
-	@Size(max = 20)
-	@Column(name = "mobile_phone", length = 20)
-	private String mobilePhone;
+    @Size(max = 20)
+    private String mobilePhone;
 
 }
