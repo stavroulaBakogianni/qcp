0c7e24a6bee8ab1af7a779238431ec8f6d85d28e
matinabozardeni <matinabozardeni@bozardeni-nb.station>
2024-10-17 17:49:24 +0300

    loggings in resource

--- src/main/java/org/acme/resource/CustomerResource.java
+++ src/main/java/org/acme/resource/CustomerResource.java
@@ -1,6 +1,7 @@
 package org.acme.resource;
 
-import static com.arjuna.ats.arjuna.logging.tsLogger.logger;
+import org.jboss.logging.Logger;
+import static io.quarkus.arc.ComponentsProvider.LOG;
 import java.util.List;
 import java.util.Optional;
 import javax.enterprise.context.RequestScoped;
@@ -35,27 +36,33 @@ public class CustomerResource {
     @Path("/customers")
     @GET
     public Response getCustomers() {
+        LOG.info("Feching All Customers");
         List<CustomerDTO> customers = customerServiceImpl.findAll();
         if (customers.isEmpty()) {
+            LOG.info("No Customers Found");
             return Response.status(Response.Status.NO_CONTENT).build();
         }
+        LOG.info("Returning Customers");
         return Response.ok(customers).build();
     }
 
     @Path("/{vat}")
     @GET
     public Response getCustomerByVat(@PathParam("vat") String vat) {
+        LOG.info("Feching Customer with vat: " + vat);
         try {
             Optional<CustomerDTO> customer = customerServiceImpl.findByVat(vat);
             if (customer.isPresent()) {
+                LOG.info("Excisting Customer");
                 return Response.ok(customer.get()).build();
             } else {
+                LOG.info("Not Excisting Customer");
                 return Response.status(Response.Status.NOT_FOUND)
                         .entity("Customer with VAT " + vat + " not found.")
                         .build();
             }
         } catch (Exception e) {
-            logger.error("Error while fetching customer by VAT: {}", e.getMessage(), e);
+            LOG.info("Error while fetching customer by VAT: {}" + vat, e.getMessage(), e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("An error occurred while fetching the customer.")
                     .build();
@@ -65,7 +72,9 @@ public class CustomerResource {
     @Path("/customer")
     @POST
     public Response saveCustomer(CustomerDTO customerDTO) {
+        LOG.info("Trying to create new customer");
         if (customerDTO == null || customerDTO.getVat() == null) {
+            LOG.info("Customer is empty");
             return Response.status(Response.Status.BAD_REQUEST)
                     .entity("Customer data or VAT number is missing.")
                     .build();
@@ -74,27 +83,29 @@ public class CustomerResource {
         try {
             ValidateVatResponse vatResponse = validateService.checkVat("EL", customerDTO.getVat());
             
-            System.out.println("VAT validation response: " + vatResponse);
 
             if (customerServiceImpl.findByVat(customerDTO.getVat()).isPresent()) {
+                LOG.info("Existing vat");
                 return Response.status(Response.Status.CONFLICT)
                         .entity("Customer with this VAT already exists.")
                         .build();
             }
 
             if (!vatResponse.getValid()) {
+                LOG.info("Invalid vat");
                 return Response.status(Response.Status.BAD_REQUEST)
                         .entity("Invalid VAT number.")
                         .build();
             }
 
             customerServiceImpl.saveCustomer(customerDTO);
+            LOG.info("Customer created");
             return Response.status(Response.Status.CREATED)
                     .entity("Customer saved successfully.")
                     .build();
 
         } catch (Exception e) {
-            logger.error("Error while saving customer: {}", e.getMessage(), e);
+            LOG.info("Error while saving customer", e.getMessage(), e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("An error occurred while saving the customer.")
                     .build();
